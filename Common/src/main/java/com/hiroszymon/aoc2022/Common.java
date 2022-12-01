657d274ed37f8e2cb6cb359a29992164b1ae067d
Szymon Rakowski <szymekdo@gmail.com>
2022-12-01 14:18:44 +0100

    Day 1 solved

---
+++ Common/src/main/java/com/hiroszymon/aoc2022/Common.java
@@ -0,0 +1,18 @@
+package com.hiroszymon.aoc2022;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+public class Common {
+    public static List<String> readInput() {
+        try {
+            return Files.readAllLines(Path.of("input.txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+            return Collections.emptyList();
+        }
+    }
+}
---
+++ Day1/target/generated-sources/archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/basic.java
@@ -0,0 +1,25 @@
+package it.pkg;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class basic {
+    private static final ArrayList<String> input = new ArrayList<>(Common.readInput());
+
+    public static void main(String[] args) {
+        part1(input);
+        part2(input);
+    }
+
+    private static void part1(ArrayList<String> input) {
+
+    }
+
+    private static void part2(ArrayList<String> input) {
+
+    }
+
+
+}
